133422f428ad4659912e2bdf83c2ccd86bd79cfb
Ihor <i.v.levin@student.khai.edu>
2022-06-25 18:16:21 +0300

    макет Хедера и Общей информации

--- components/myHeader.vue
+++ components/myHeader.vue
@@ -1,10 +1,54 @@
 <template>
-<h1>Header</h1>
+  <header>
+    <div class="logo">
+      <span> Levin Ihor </span>
+    </div>
+    <div class="navbar">
+      <div class="link">Info</div>
+      <div class="link">Projects</div>
+      <div class="link">Links</div>
+      <div class="link">Footer</div>
+      <my-toggle />
+    </div>
+  </header>
 </template>
+
 <script>
 export default {
-  name: "MyHeader"
+  name: "MyHeader",
 };
 </script>
 
-
+<style scoped>
+.logo {
+  font-size: 40px;
+}
+img {
+  width: 100%;
+  height: 100%;
+  contain: cover;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 20px;
+}
+.navbar {
+  width: 40%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.link{
+  text-decoration: none;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #000;
+  cursor: pointer;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -17,23 +17,7 @@
 ::-webkit-scrollbar {
   width: 0;
 }
-body {
-	background: linear-gradient(-45deg, #464646, #D54E21, #21759B, #23d5ab);
-	background-size: 400% 400%;
-	animation: gradient 15s ease infinite;
-	height: 100vh;
-	}
-
-@keyframes gradient {
-	0% {
-		background-position: 0% 50%;
-	}
-	50% {
-		background-position: 100% 50%;
-	}
-	100% {
-		background-position: 0% 50%;
-	}
+body{
+	background-color: black;
 }
-
 </style>
